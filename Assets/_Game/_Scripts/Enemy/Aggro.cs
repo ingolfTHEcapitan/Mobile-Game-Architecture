80570b03d3ebfb1102e59a53f0d6748013d9ed5b
ingolfTHEcapitan <vitaly.litov4encko2017@gmail.com>
2025-07-21 22:14:23 +1000

    feat(enemy): реализована зона агра противников
    Противники преследуют героя, пока он в зоне агра и некоторое время после выхода из неё

---
+++ Assets/_Game/_Scripts/Enemy/Aggro.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+
+namespace _Game._Scripts.Enemy
+{
+    public class Aggro: MonoBehaviour
+    {
+        [SerializeField] private TriggerObserver _triggerObserver;
+        [SerializeField] private AgentMoveToPlayer _follow;
+        [SerializeField] private float _cooldown;
+        
+        private Coroutine _agroCoroutine;
+        private bool _hasAgroTarget;
+
+        private void Start()
+        {
+            _triggerObserver.TriggerEnter += OnTriggerEnter;
+            _triggerObserver.TriggerExit += OnTriggerExit;
+
+            SwitchFollowOff();
+        }
+
+        private void OnTriggerEnter(Collider obj)
+        {
+            if (!_hasAgroTarget)
+            {
+                _hasAgroTarget = true;
+                
+                StopAgroCoroutine();
+                SwitchFollowOn();
+            }
+        }
+
+        private void OnTriggerExit(Collider obj)
+        {
+            if (_hasAgroTarget)
+            {
+                _hasAgroTarget = false;
+                
+                _agroCoroutine = StartCoroutine(SwitchFollowOffAfterCooldown());
+            }
+        }
+
+        private void StopAgroCoroutine()
+        {
+            if (_agroCoroutine != null)
+            {
+                StopCoroutine(_agroCoroutine);
+                _agroCoroutine = null;
+            }
+        }
+
+        private IEnumerator SwitchFollowOffAfterCooldown()
+        {
+            yield return new WaitForSeconds(_cooldown);
+            SwitchFollowOff();
+        }
+
+        private void SwitchFollowOff() => 
+            _follow.enabled = false;
+
+        private void SwitchFollowOn() => 
+            _follow.enabled = true;
+    }
+}
