695ed4b896b5ffea869b7e62fe609d35065c1d29
ingolfTHEcapitan <vitaly.litov4encko2017@gmail.com>
2025-08-01 13:48:59 +1000

    chore(prefabs): все префабы были перенесены в папку Resources

--- Assets/_Game/_Scripts/Infrastructure/AssetManagement/AssetPath.cs
+++ Assets/_Game/_Scripts/Infrastructure/AssetManagement/AssetPath.cs
@@ -3,6 +3,6 @@ namespace _Game._Scripts.Infrastructure.AssetManagement
     public static class AssetPath
     {
         public const string HeroPath = "Prefabs/Hero";
-        public const string HudPath = "Prefabs/Hud";
+        public const string HudPath = "Prefabs/UI/HUD/Hud";
     }
 }
