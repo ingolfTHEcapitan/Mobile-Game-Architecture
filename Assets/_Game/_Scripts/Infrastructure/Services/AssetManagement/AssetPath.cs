8be394da27fa94f31db578c3ade1f894170f9b94
ingolfTHEcapitan <vitaly.litov4encko2017@gmail.com>
2025-09-02 15:18:17 +1000

    feat(ui): собран интерфейс магазина

--- Assets/_Game/_Scripts/Infrastructure/Services/AssetManagement/AssetPath.cs
+++ Assets/_Game/_Scripts/Infrastructure/Services/AssetManagement/AssetPath.cs
@@ -3,7 +3,7 @@ namespace _Game._Scripts.Infrastructure.Services.AssetManagement
     public static class AssetPath
     {
         public const string Hero = "Prefabs/Hero/Hero";
-        public const string Hud = "Prefabs/UI/HUD/Hud";
+        public const string Hud = "Prefabs/UI/HUD/HUDLayer";
         public const string Loot = "Prefabs/Loot";
         public const string EnemySpawner = "Prefabs/EnemySpawner/EnemySpawner";
     }
